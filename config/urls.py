c9b086cc486b45ad188d251dc9cea5b36b2ad96c
Rahma Eliasu <eliasurahma@gmail.com>
2025-12-07 16:15:42 +0000

    Feat: Add login endpoints

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 
     # API Routes
     path('api/', include('apps.catalog.urls')),
+
+    # Auth Routes
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 
