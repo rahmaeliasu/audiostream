fc08e0cf9a60fedd1864a0012d7af301b10c9b58
Rahma Eliasu <eliasurahma@gmail.com>
2025-12-07 17:08:45 +0000

    Chore: Generate new secrets and move to environment variables

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,7 +11,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
 from datetime import timedelta
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -21,10 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-cgn6_z624e0#^2z0b)hig!7jwg48ra!wr!ez0(@ssjjvfqc$fo"
+# fetch it from the environment
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# check if the env variable is literally the string "True"
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
